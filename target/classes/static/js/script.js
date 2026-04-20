e2d830cf122c6d50de0265a9ce09acdcf5d4c61e
Lucas Hoffmann <loegcas@gmail.com>
2026-04-20 21:17:05 +0200

    added sidebar and homepage design

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,25 @@
+function updateClock() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById("clock").textContent =
+        `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+document.querySelectorAll(".selectorImage").forEach(img => {
+    img.addEventListener("click", function () {
+
+        document.querySelectorAll(".selectorImage")
+            .forEach(i => i.classList.remove("active"));
+
+        this.classList.add("active");
+
+        document.getElementById("portraitImage").src = this.src;
+    });
+});
